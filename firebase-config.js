0ff7b861434f1d45a43f5519248c577e4396873e
ruelpalcon <westernianprogrammer@gmail.com>
2025-03-30 19:58:50 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,19 @@
-// Import Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your Firebase Configuration
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDHZIUnGP7JLpQJ3oWrdb1tgI-8AI9VmJo",
+    authDomain: "rpfx-b7288.firebaseapp.com",
+    projectId: "rpfx-b7288",
+    storageBucket: "rpfx-b7288.firebasestorage.app",
+    messagingSenderId: "592595389242",
+    appId: "1:592595389242:web:a7d9baa7748086b29f5e03"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// Export auth for use in other files
-export { auth };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
