2fc323e5888c4c1bb3d1e3bfda611cd6d3aa9c8a
ruelpalcon <westernianprogrammer@gmail.com>
2025-03-30 14:26:29 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,21 @@
-// firebase-config.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAiyTxPvkH-Pafr1vg-tVEbI7adKWWSmnA",
+  authDomain: "rpfx-trading.firebaseapp.com",
+  projectId: "rpfx-trading",
+  storageBucket: "rpfx-trading.firebasestorage.app",
+  messagingSenderId: "1018093031159",
+  appId: "1:1018093031159:web:02309e1537adb65e664df2",
+  measurementId: "G-Y1Q14V72TZ"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
